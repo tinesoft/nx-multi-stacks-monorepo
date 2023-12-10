10e85d017d6e7a3176fd82a8f44aa2157d9fc92d
Tine Kondo <kondotine@gmail.com>
2023-12-10 19:41:05 +0100

    chore(angular): generate a  basic `welcome` angular component in the `nglib` library

---
+++ frontend/libs/nglib/components/welcome.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nglib-welcome',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WelcomeComponent {}
