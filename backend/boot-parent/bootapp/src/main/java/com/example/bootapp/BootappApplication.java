aee140567558925103dae17894c3cc67d9efcb6b
Tine Kondo <kondotine@gmail.com>
2023-12-12 07:08:49 +0100

    chore(boot): generate a basic spring boot application

---
+++ backend/boot-parent/bootapp/src/main/java/com/example/bootapp/BootappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootappApplication.class, args);
+	}
+
+}
