965924b81e35287cbaf21040ac9508ca0448abac
Tine Kondo <kondotine@gmail.com>
2023-12-12 08:55:10 +0100

    chore(boot): update the `bootapp` to use the `bootlib`

--- backend/boot-parent/bootapp/src/main/java/com/example/bootapp/BootappApplication.java
+++ backend/boot-parent/bootapp/src/main/java/com/example/bootapp/BootappApplication.java
@@ -3,7 +3,7 @@ package com.example.bootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
 public class BootappApplication {
 
 	public static void main(String[] args) {
--- backend/boot-parent/bootapp/src/main/java/com/example/bootapp/controller/WelcomeController.java
+++ backend/boot-parent/bootapp/src/main/java/com/example/bootapp/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.example.bootapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.bootlib.service.MyService;
 
 @RestController("/api")
 public class WelcomeController {
@@ -10,8 +11,14 @@ public class WelcomeController {
 	private static final String WELCOME_TEMPLATE = "Welcome, %s, from '%s'!";
     public static record WelcomeMessage(String user, String message) { }
 
+    private final MyService myService;
+
+	public WelcomeController(MyService myService) {
+		this.myService = myService;
+	}
+
 	@GetMapping("/welcome")
 	public WelcomeMessage welcome(@RequestParam(value = "user", defaultValue = "Sfeir School Nx Attendee") String user) {
-		return new WelcomeMessage(user, String.format(WELCOME_TEMPLATE, user, "bootapp"));
+		return new WelcomeMessage(user, String.format(WELCOME_TEMPLATE, user, this.myService.message()));
 	}
 }
